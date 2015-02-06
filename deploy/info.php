6cb6b09c40b758c8b08c2ec87e1ab7ebd0a8970a
pcbaldwin <pbaldwin@clearfoundation.com>
2015-02-05 21:05:21 -0500

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'devel';
-$app['version'] = '2.0.10';
+$app['version'] = '2.0.18';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-devel.spec
+++ packaging/app-devel.spec
@@ -1,7 +1,7 @@
 
 Name: app-devel
 Epoch: 1
-Version: 2.0.10
+Version: 2.0.18
 Release: 1%{dist}
 Summary: Developer Tools
 License: GPLv3
