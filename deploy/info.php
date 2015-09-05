6cfeb8500654cc27db03fc9f44f9d64037c0eea0
pcbaldwin <pbaldwin@clearfoundation.com>
2015-09-04 23:41:05 -0400

    Update default SSL settings [tracker #4832]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'devel';
-$app['version'] = '2.1.7';
+$app['version'] = '2.1.20';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
