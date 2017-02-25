81f2686bb5013a0c351c1541767f27cff2bd1d32
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:09:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'garapen tresnak';
+$lang['devel_app_description'] = 'Orri hau azkar sustatzailearen gaia eta beste widgets errepaso bat ematen du.';
+$lang['devel_install_wizard'] = 'Instalatu morroia';
+$lang['devel_install_wizard_help'] = 'Egin klik honako esteka instalatzeko morroia abiarazteko.';
+$lang['devel_theme_viewer'] = 'gaiaren ikustailea';
+$lang['devel_theme_viewer_help'] = 'Egin klik honako esteka gaiaren ikuskatzea.';
+$lang['devel_translations'] = 'Itzulpenak';
+$lang['devel_translation_mode'] = 'Itzulpena Mode';
+$lang['devel_view_theme'] = 'ikusi Theme';
+$lang['devel_synchronize_updates'] = 'Sinkronizatu eguneraketak';
+$lang['devel_synchronization_state_invalid'] = 'Sinkronizazioa egoera baliogabea da.';
