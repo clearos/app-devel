53539669b6c29ce2648d2396ddb081b3bc543dcd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:18:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Tools developer indonesia';
+$lang['devel_app_description'] = 'leqepheng lena e fana ka potlakela developer indonesia kakaretso ea sehlooho se reng &#39;me diwidget tse ling.';
+$lang['devel_install_wizard'] = 'kenya wizate';
+$lang['devel_install_wizard_help'] = 'Tobetsa link tsa latelang ho qala kenya wizate.';
+$lang['devel_theme_viewer'] = 'Theme mmogedi o';
+$lang['devel_theme_viewer_help'] = 'Tobetsa link tsa latelang ho hlahloba sehlooho se reng.';
+$lang['devel_translations'] = 'liphetolelo';
+$lang['devel_translation_mode'] = 'Phetolelo Mode';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'amahanya mabitso Updates';
+$lang['devel_synchronization_state_invalid'] = 'Kamahanyo puso ha o sebetse.';
