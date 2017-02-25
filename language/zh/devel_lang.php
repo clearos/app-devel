c8af66c6a540f61ace4e8c4d97addf7e614ecdda
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:10:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = '开发者工具';
+$lang['devel_app_description'] = '该页面提供的主题和其他部件的快速开发概述。';
+$lang['devel_install_wizard'] = '安装向导';
+$lang['devel_install_wizard_help'] = '点击以下链接来启动安装向导。';
+$lang['devel_theme_viewer'] = '主题查看器';
+$lang['devel_theme_viewer_help'] = '点击以下链接查看的主题。';
+$lang['devel_translations'] = '翻译';
+$lang['devel_translation_mode'] = '转换模式';
+$lang['devel_view_theme'] = '查看主题';
+$lang['devel_synchronize_updates'] = '同步更新';
+$lang['devel_synchronization_state_invalid'] = '同步状态无效。';
