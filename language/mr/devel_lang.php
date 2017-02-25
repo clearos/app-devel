7c3de8eb02aef408bab1e260487a3380ad6c5779
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:15:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'विकसक साधने';
+$lang['devel_app_description'] = 'या पृष्ठावरील थीम आणि इतर विजेट एक द्रुत विकसक पुरवते.';
+$lang['devel_install_wizard'] = 'सहाय्यक स्थापित';
+$lang['devel_install_wizard_help'] = 'स्थापना विझार्ड सुरू करण्यासाठी खालील दुव्यावर क्लिक करा.';
+$lang['devel_theme_viewer'] = 'थीम दर्शक';
+$lang['devel_theme_viewer_help'] = 'थीम पुनरावलोकन करण्यासाठी खालील लिंक वर क्लिक करा.';
+$lang['devel_translations'] = 'अनुवाद';
+$lang['devel_translation_mode'] = 'भाषांतर मोड';
+$lang['devel_view_theme'] = 'थीम पहा';
+$lang['devel_synchronize_updates'] = 'समजुळणी सुधारणा';
+$lang['devel_synchronization_state_invalid'] = 'समक्रमण राज्य अवैध आहे.';
