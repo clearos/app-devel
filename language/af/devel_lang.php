e92b7f4f77fe9945ba070eb810090383da79aa73
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:08:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Ontwikkel Tools';
+$lang['devel_app_description'] = 'Hierdie bladsy bied &#39;n vinnige ontwikkelaar oorsig van die tema en ander widgets.';
+$lang['devel_install_wizard'] = 'Install Wizard';
+$lang['devel_install_wizard_help'] = 'Klik op die volgende skakel om die towenaar installeer begin.';
+$lang['devel_theme_viewer'] = 'tema Viewer';
+$lang['devel_theme_viewer_help'] = 'Klik op die volgende skakel om die tema te hersien.';
+$lang['devel_translations'] = 'vertalings';
+$lang['devel_translation_mode'] = 'vertaling af';
+$lang['devel_view_theme'] = 'Kyk Tema';
+$lang['devel_synchronize_updates'] = 'sinchroniseer Updates';
+$lang['devel_synchronization_state_invalid'] = 'Synchronisatie staat is ongeldig.';
