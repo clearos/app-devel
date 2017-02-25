2594ae6e19df4a379fe5eeb1fbe0dc457ffc6d9d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:20:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Những công cụ phát triển';
+$lang['devel_app_description'] = 'Trang này cung cấp một cái nhìn tổng quan phát triển nhanh chóng của các chủ đề và các vật dụng khác.';
+$lang['devel_install_wizard'] = 'cài đặt wizard';
+$lang['devel_install_wizard_help'] = 'Click vào liên kết sau đây để bắt đầu wizard cài đặt.';
+$lang['devel_theme_viewer'] = 'Viewer Theme';
+$lang['devel_theme_viewer_help'] = 'Click vào link sau để xem các chủ đề.';
+$lang['devel_translations'] = 'Dịch';
+$lang['devel_translation_mode'] = 'dịch Chế độ';
+$lang['devel_view_theme'] = 'Xem Theme';
+$lang['devel_synchronize_updates'] = 'Đồng bộ hóa cập nhật';
+$lang['devel_synchronization_state_invalid'] = 'nhà nước đồng bộ là không hợp lệ.';
