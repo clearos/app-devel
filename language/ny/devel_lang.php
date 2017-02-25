8b470787152f18b4ca5919dac0bbf0ac28c820c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:10:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Zida mapulogalamu';
+$lang['devel_app_description'] = 'tsamba lino amapereka mwamsanga mapulogalamu mwachidule phunziro lathu ndi zida zina.';
+$lang['devel_install_wizard'] = 'kukhazikitsa mfiti';
+$lang['devel_install_wizard_help'] = 'Dinani pa linki zotsatirazi kuyamba kukhazikitsa mfiti.';
+$lang['devel_theme_viewer'] = 'mutu mmodzi';
+$lang['devel_theme_viewer_help'] = 'Dinani pa linki zotsatirazi kubwerezanso phunziro lathu.';
+$lang['devel_translations'] = 'Kumasulira';
+$lang['devel_translation_mode'] = 'Translation Mafilimu angaphunzitse';
+$lang['devel_view_theme'] = 'View Mutu';
+$lang['devel_synchronize_updates'] = 'synchronize Zosintha';
+$lang['devel_synchronization_state_invalid'] = 'Kalunzanitsidwe boma ili udzakhala osazindikilika.';
