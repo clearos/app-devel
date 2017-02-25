ec5d2cf103b7d2ab28411c0427113019a4e49fb9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:15:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Alat Pembangun';
+$lang['devel_app_description'] = 'Laman ini memberikan gambaran keseluruhan pemaju cepat daripada tema dan widget lain.';
+$lang['devel_install_wizard'] = 'Pasang Wizard';
+$lang['devel_install_wizard_help'] = 'Klik pada pautan berikut untuk memulakan wizard pemasangan.';
+$lang['devel_theme_viewer'] = 'Viewer tema';
+$lang['devel_theme_viewer_help'] = 'Klik pada pautan berikut untuk menyemak tema.';
+$lang['devel_translations'] = 'terjemahan';
+$lang['devel_translation_mode'] = 'Mod terjemahan';
+$lang['devel_view_theme'] = 'Lihat Tema';
+$lang['devel_synchronize_updates'] = 'Menyegerak Maklumat Terbaru';
+$lang['devel_synchronization_state_invalid'] = 'negeri Penyegerakan tidak sah.';
