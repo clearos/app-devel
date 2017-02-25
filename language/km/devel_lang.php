08bed513c54b8afaae2cb3ee2ddb4e76aec1f095
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:13:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'ឧបករណ៍អភិវឌ្ឍន៍';
+$lang['devel_app_description'] = 'ទំព័រនេះផ្តល់នូវទិដ្ឋភាពទូទៅការអភិវឌ្ឍឆាប់រហ័សនៃស្បែកនិងធាតុក្រាហ្វិកផ្សេងទៀត។';
+$lang['devel_install_wizard'] = 'អ្នកជំនួយការដំឡើង';
+$lang['devel_install_wizard_help'] = 'សូមចុចលើតំណខាងក្រោមដើម្បីចាប់ផ្តើមអ្នកជំនួយការដំឡើង។';
+$lang['devel_theme_viewer'] = 'កម្មវិធីមើលស្បែក';
+$lang['devel_theme_viewer_help'] = 'សូមចុចលើតំណខាងក្រោមដើម្បីពិនិត្យស្បែក។';
+$lang['devel_translations'] = 'ការបកប្រែ';
+$lang['devel_translation_mode'] = 'របៀបបកប្រែ';
+$lang['devel_view_theme'] = 'មើលស្បែក';
+$lang['devel_synchronize_updates'] = 'ធ្វើសមកាលកម្មការធ្វើឱ្យទាន់សម័យ';
+$lang['devel_synchronization_state_invalid'] = 'រដ្ឋធ្វើសមកាលកម្មមិនត្រឹមត្រូវ។';
