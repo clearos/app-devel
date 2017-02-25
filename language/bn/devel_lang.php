9cb9812fffb66a69aecbe8fe0f3cd7ba08ed19b2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:09:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'ডেভেলপার টুলস';
+$lang['devel_app_description'] = 'এ পৃষ্ঠায় থিম এবং অন্যান্য উইজেট এর একটি দ্রুত ডেভেলপার ওভারভিউ উপলব্ধ.';
+$lang['devel_install_wizard'] = 'উইজার্ড ইনস্টল';
+$lang['devel_install_wizard_help'] = 'নিম্নলিখিত লিঙ্কে ক্লিক করুন উইজার্ড ইনস্টল শুরু করার জন্য.';
+$lang['devel_theme_viewer'] = 'থিম ভিউয়ার';
+$lang['devel_theme_viewer_help'] = 'নিম্নলিখিত লিঙ্কে ক্লিক করুন থিম পর্যালোচনা করতে.';
+$lang['devel_translations'] = 'অনুবাদগুলো';
+$lang['devel_translation_mode'] = 'অনুবাদ মোড';
+$lang['devel_view_theme'] = 'থিম দেখুন';
+$lang['devel_synchronize_updates'] = 'সিংক্রোনাইজ আপডেট';
+$lang['devel_synchronization_state_invalid'] = 'সুংসগতি রাষ্ট্র অবৈধ.';
