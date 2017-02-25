90b4dcc19200f5b1e8d49e533e457a95c57c7a5e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:14:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'ເຄື່ອງມືພັດທະນາ';
+$lang['devel_app_description'] = 'ຫນ້ານີ້ຈະສະຫນອງສະພາບລວມການພັດທະນາໄວຂອງຫົວຂໍ້ແລະເຄື່ອງມືອື່ນ ໆ .';
+$lang['devel_install_wizard'] = 'ການຕິດຕັ້ງຂອງຄວາມຍາວປາ';
+$lang['devel_install_wizard_help'] = 'ໃຫ້ຄລິກໃສ່ການເຊື່ອມຕໍ່ດັ່ງຕໍ່ໄປນີ້ເພື່ອເລີ່ມຕົ້ນການຂອງຄວາມຍາວປາການຕິດຕັ້ງ.';
+$lang['devel_theme_viewer'] = 'Viewer ທີມ';
+$lang['devel_theme_viewer_help'] = 'ໃຫ້ຄລິກໃສ່ການເຊື່ອມຕໍ່ດັ່ງຕໍ່ໄປນີ້ການທົບທວນຄືນຫົວຂໍ້.';
+$lang['devel_translations'] = 'ການແປພາສາ';
+$lang['devel_translation_mode'] = 'Mode ການແປພາສາ';
+$lang['devel_view_theme'] = 'ເບິ່ງຫົວຂໍ້';
+$lang['devel_synchronize_updates'] = 'ການປັບປຸງຕາຫນ່າງ';
+$lang['devel_synchronization_state_invalid'] = 'ລັດ Synchronization ແມ່ນບໍ່ຖືກຕ້ອງ.';
