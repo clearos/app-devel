4ce6ba0b47ef0d8d4ccf339bea2a4a4be919fd0b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:16:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'ابزار های برنامه نویسی';
+$lang['devel_app_description'] = 'این صفحه مروری توسعه سریع از موضوع و دیگر ویدجت فراهم می کند.';
+$lang['devel_install_wizard'] = 'نصب جادوگر';
+$lang['devel_install_wizard_help'] = 'با کلیک بر روی لینک زیر برای شروع به جادوگر نصب کنید.';
+$lang['devel_theme_viewer'] = 'تم بیننده';
+$lang['devel_theme_viewer_help'] = 'بر روی لینک زیر کلیک کنید به بررسی موضوع است.';
+$lang['devel_translations'] = 'ترجمه';
+$lang['devel_translation_mode'] = 'حالت ترجمه';
+$lang['devel_view_theme'] = 'نمایش موضوع';
+$lang['devel_synchronize_updates'] = 'همگام سازی به روز رسانی';
+$lang['devel_synchronization_state_invalid'] = 'دولت هماهنگ سازی نامعتبر است.';
