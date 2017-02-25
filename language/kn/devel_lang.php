010aa92cac805f255931d3636430cfc5a80f775a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:14:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'ಡೆವಲಪರ್ ಟೂಲ್ಸ್';
+$lang['devel_app_description'] = 'ಈ ಪುಟ ಥೀಮ್ ಮತ್ತು ಇತರೆ widget ಒಂದು ತ್ವರಿತ ಡೆವಲಪರ್ ಅವಲೋಕನ ಒದಗಿಸುತ್ತದೆ.';
+$lang['devel_install_wizard'] = 'ಅನುಸ್ಥಾಪನಾ ವಿಝಾರ್ಡ್';
+$lang['devel_install_wizard_help'] = 'ಅನುಸ್ಥಾಪಿಸಲು ಮಾಂತ್ರಿಕ ಆರಂಭಿಸಲು ಕೆಳಗಿನ ಲಿಂಕ್ ಕ್ಲಿಕ್ ಮಾಡಿ.';
+$lang['devel_theme_viewer'] = 'ಥೀಮ್ ವೀಕ್ಷಕ';
+$lang['devel_theme_viewer_help'] = 'ಥೀಮ್ ಪರಿಶೀಲಿಸಲು ಕೆಳಗಿನ ಲಿಂಕ್ ಕ್ಲಿಕ್ ಮಾಡಿ.';
+$lang['devel_translations'] = 'ಅನುವಾದಗಳು';
+$lang['devel_translation_mode'] = 'ಅನುವಾದ ಮೋಡ್';
+$lang['devel_view_theme'] = 'ಥೀಮ್ ವೀಕ್ಷಿಸಿ';
+$lang['devel_synchronize_updates'] = 'ಸಿಂಕ್ರೊನೈಸ್ ಅಪ್ಡೇಟ್ಗಳು';
+$lang['devel_synchronization_state_invalid'] = 'ಸಿಂಕ್ರೊನೈಸೇಶನ್ ರಾಜ್ಯದ ಅಮಾನ್ಯವಾಗಿದೆ.';
