f20ab19fa45b1b0cf08acd20d52ed33a5572f87d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:19:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Tools ташкилкунанда';
+$lang['devel_app_description'] = 'Ин саҳифа шарҳи тањиягари зуд аз мавзӯъ ва паҳни дигар таъмин менамояд.';
+$lang['devel_install_wizard'] = 'Install Ёвари';
+$lang['devel_install_wizard_help'] = 'Ангушт занед дар пайванд зерин барои оғози устоди насб.';
+$lang['devel_theme_viewer'] = 'мавзӯъи Навид';
+$lang['devel_theme_viewer_help'] = 'дар пайванд зерин пахш кунед барои баррасии мавзӯъ.';
+$lang['devel_translations'] = 'тарҷумаҳо';
+$lang['devel_translation_mode'] = 'Ҳолати Translation';
+$lang['devel_view_theme'] = 'View мавзӯъ';
+$lang['devel_synchronize_updates'] = 'ҳамоҳангсозии Шарҳҳо';
+$lang['devel_synchronization_state_invalid'] = 'давлатии Ҳамоҳангсозӣ нодуруст аст.';
