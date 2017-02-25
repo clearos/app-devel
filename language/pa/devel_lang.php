5ce4a56e232c2c461bd0acf3d9dcf354b59680ac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:16:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'ਡਿਵੈਲਪਰ ਟੂਲ';
+$lang['devel_app_description'] = 'ਇਹ ਸਫ਼ਾ ਥੀਮ ਅਤੇ ਹੋਰ ਵਿਦਜਿਟ ਦੀ ਇੱਕ ਤੇਜ਼ ਡਿਵੈਲਪਰ ਸੰਖੇਪ ਜਾਣਕਾਰੀ ਦਿੰਦਾ ਹੈ.';
+$lang['devel_install_wizard'] = 'ਸਹਾਇਕ ਇੰਸਟਾਲ ਕਰੋ';
+$lang['devel_install_wizard_help'] = 'ਹੇਠ ਦਿੱਤੇ ਲਿੰਕ &#39;ਤੇ ਕਲਿੱਕ ਕਰੋ ਸਹਾਇਕ ਇੰਸਟਾਲ ਸ਼ੁਰੂ ਕਰਨ ਲਈ.';
+$lang['devel_theme_viewer'] = 'ਥੀਮ ਦਰਸ਼ਕ';
+$lang['devel_theme_viewer_help'] = 'ਹੇਠ ਦਿੱਤੇ ਲਿੰਕ &#39;ਤੇ ਕਲਿੱਕ ਕਰੋ ਥੀਮ ਦੀ ਸਮੀਖਿਆ ਕਰਨ ਲਈ.';
+$lang['devel_translations'] = 'ਅਨੁਵਾਦ';
+$lang['devel_translation_mode'] = 'ਅਨੁਵਾਦ ਮੋਡ';
+$lang['devel_view_theme'] = 'ਥੀਮ ਦੇਖੋ';
+$lang['devel_synchronize_updates'] = 'ਸਮਕਾਲੀ ਅੱਪਡੇਟ';
+$lang['devel_synchronization_state_invalid'] = 'ਸਮਕਾਲੀ ਰਾਜ ਦੇ ਗਲਤ ਹੈ.';
