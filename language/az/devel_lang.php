c3e8139ee8e4d4fcfea75bcf7e521559f18affb3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:09:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Developer Tools';
+$lang['devel_app_description'] = 'Bu səhifə mövzu və digər widgets sürətli geliştirici bakış təmin edir.';
+$lang['devel_install_wizard'] = 'Wizard Yükle';
+$lang['devel_install_wizard_help'] = 'yüklemek sehrbazdır başlamaq üçün aşağıdakı linki basın.';
+$lang['devel_theme_viewer'] = 'Theme Viewer';
+$lang['devel_theme_viewer_help'] = 'mövzu nəzərdən aşağıdakı linki basın.';
+$lang['devel_translations'] = 'Translations';
+$lang['devel_translation_mode'] = 'Tərcümə Mode';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'sinxronizasiya Updates';
+$lang['devel_synchronization_state_invalid'] = 'Sinxronizasiya dövlət yanlışdır.';
