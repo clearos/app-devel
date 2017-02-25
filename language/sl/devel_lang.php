68868375f1311899ec01302b1fca7a8ca3721126
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:17:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'orodja za razvijalce';
+$lang['devel_app_description'] = 'Ta stran vsebuje pregled hitro razvijalsko temi in drugimi pripomočki.';
+$lang['devel_install_wizard'] = 'namestitev Wizard';
+$lang['devel_install_wizard_help'] = 'Kliknite na spodnjo povezavo, da zaženete čarovnika za namestitev.';
+$lang['devel_theme_viewer'] = 'tema Viewer';
+$lang['devel_theme_viewer_help'] = 'Kliknite na spodnjo povezavo, da pregleda temo.';
+$lang['devel_translations'] = 'prevodi';
+$lang['devel_translation_mode'] = 'prevod način';
+$lang['devel_view_theme'] = 'Poglej Tema';
+$lang['devel_synchronize_updates'] = 'Sinhronizacija Posodobitve';
+$lang['devel_synchronization_state_invalid'] = 'Sinhronizacija stanje je neveljavna.';
