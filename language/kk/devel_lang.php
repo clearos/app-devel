f3da6ce9ecfd38ce857c707960fc6e416a6b47a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:13:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Developer Tools';
+$lang['devel_app_description'] = 'Бұл бет тақырыбы және басқа да виджеттерді жылдам әзірлеуші ​​шолу береді.';
+$lang['devel_install_wizard'] = 'шеберін орнату';
+$lang['devel_install_wizard_help'] = 'орнату шеберін іске қосу үшін, келесі сілтемені басыңыз.';
+$lang['devel_theme_viewer'] = 'Тақырып Viewer';
+$lang['devel_theme_viewer_help'] = 'тақырыпты қарап шығу үшін келесі сілтемеге басыңыз.';
+$lang['devel_translations'] = 'Аудармалар';
+$lang['devel_translation_mode'] = 'аударма режимі';
+$lang['devel_view_theme'] = 'қарау Тақырып';
+$lang['devel_synchronize_updates'] = 'жаңартуларын синхрондау';
+$lang['devel_synchronization_state_invalid'] = 'Қадамдастыру мемлекеттік жарамсыз болып табылады.';
