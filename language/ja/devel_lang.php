7973f41cb1367573e3d42ab24816d595ab6ea1fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:13:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = '開発者ツール';
+$lang['devel_app_description'] = 'このページでは、テーマと他のウィジェットの迅速な開発者の概要を説明します。';
+$lang['devel_install_wizard'] = 'インストールウィザードを';
+$lang['devel_install_wizard_help'] = 'インストールウィザードを起動するには、次のリンクをクリックしてください。';
+$lang['devel_theme_viewer'] = 'テーマビューア';
+$lang['devel_theme_viewer_help'] = 'テーマを確認するには、以下のリンクをクリックしてください。';
+$lang['devel_translations'] = '翻訳';
+$lang['devel_translation_mode'] = '変換モード';
+$lang['devel_view_theme'] = 'Viewテーマ';
+$lang['devel_synchronize_updates'] = '更新の同期';
+$lang['devel_synchronization_state_invalid'] = '同期の状態が無効です。';
