148f5f31532f85c410a96f4b7c515f63185c4f38
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:14:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Kurejo irankiai';
+$lang['devel_app_description'] = 'Šis puslapis leidžia greitai kūrėjas apžvalgą tema ir kiti raštai.';
+$lang['devel_install_wizard'] = 'Įdiekite Wizard';
+$lang['devel_install_wizard_help'] = 'Spustelėkite šią nuorodą, jei norite pradėti diegti vedlį.';
+$lang['devel_theme_viewer'] = 'tema peržiūros';
+$lang['devel_theme_viewer_help'] = 'Spustelėkite šią nuorodą, jei norite peržiūrėti temą.';
+$lang['devel_translations'] = 'Vertimai';
+$lang['devel_translation_mode'] = 'Vertimas režimas';
+$lang['devel_view_theme'] = 'Peržiūrėti temą';
+$lang['devel_synchronize_updates'] = 'Sinchronizacija Atnaujinimai';
+$lang['devel_synchronization_state_invalid'] = 'Sinchronizacija būsena yra negaliojantis.';
