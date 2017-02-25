cb37045302919586e3b8777f54fbc61fca664299
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:18:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'டெவலப்பர் கருவிகள்';
+$lang['devel_app_description'] = 'இப்பக்கம் தீம் மற்றும் பிற விட்ஜெட்டுகளை ஒரு விரைவான டெவலப்பர் கண்ணோட்டத்தை வழங்குகிறது.';
+$lang['devel_install_wizard'] = 'நிறுவல் வழிகாட்டி';
+$lang['devel_install_wizard_help'] = 'நிறுவ வழிகாட்டி தொடங்க பின்வரும் இணைப்பை கிளிக் செய்க.';
+$lang['devel_theme_viewer'] = 'தீம் பார்வையாளர்';
+$lang['devel_theme_viewer_help'] = 'தீம் ஆய்வு செய்ய பின்வரும் இணைப்பை கிளிக் செய்க.';
+$lang['devel_translations'] = 'மொழிபெயர்ப்பு';
+$lang['devel_translation_mode'] = 'மொழிபெயர்ப்பு முறை';
+$lang['devel_view_theme'] = 'காண்க தீம்';
+$lang['devel_synchronize_updates'] = 'ஒருங்கிணைக்கவும் மேம்படுத்தல்கள்';
+$lang['devel_synchronization_state_invalid'] = 'ஒருங்கிணைப்பு மாநில தவறானது.';
