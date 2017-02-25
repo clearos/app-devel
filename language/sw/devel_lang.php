15cffb65fb79046fcdeea0b321bad8176de94239
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:18:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Zana developer';
+$lang['devel_app_description'] = 'ukurasa huu hutoa haraka developer maelezo ya jumla ya mada na vilivyoandikwa nyingine.';
+$lang['devel_install_wizard'] = 'kufunga mchawi';
+$lang['devel_install_wizard_help'] = 'Bonyeza kiungo zifuatazo kuanza kufunga mchawi.';
+$lang['devel_theme_viewer'] = 'mandhari Viewer';
+$lang['devel_theme_viewer_help'] = 'Bonyeza kiungo zifuatazo kwa kupitia mandhari.';
+$lang['devel_translations'] = 'tafsiri';
+$lang['devel_translation_mode'] = 'tafsiri Mode';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'Synchronize Updates';
+$lang['devel_synchronization_state_invalid'] = 'Synchronization hali ni batili.';
