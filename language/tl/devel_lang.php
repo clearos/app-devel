4179d28ba771f2da053196ca1105055e09470782
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:19:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Tools Developer';
+$lang['devel_app_description'] = 'Ang pahinang ito ay nagbibigay ng isang mabilis na pangkalahatang-ideya developer ng tema at iba pang mga widgets.';
+$lang['devel_install_wizard'] = 'I-install Wizard';
+$lang['devel_install_wizard_help'] = 'Mag-click sa sumusunod na link upang simulan ang pag-install wizard.';
+$lang['devel_theme_viewer'] = 'Theme Viewer';
+$lang['devel_theme_viewer_help'] = 'Mag-click sa sumusunod na link upang suriin ang mga tema.';
+$lang['devel_translations'] = 'pagsasalin';
+$lang['devel_translation_mode'] = 'translation Mode';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'I-synchronize ang Update';
+$lang['devel_synchronization_state_invalid'] = 'Synchronization estado ay hindi wasto.';
