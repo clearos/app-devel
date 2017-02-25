e8ecc074714d23c41e8bc43d16a4abd53458b616
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:19:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Інструменти для розробників';
+$lang['devel_app_description'] = 'На цій сторінці представлений короткий огляд розробник теми і інших віджетів.';
+$lang['devel_install_wizard'] = 'Майстер установки';
+$lang['devel_install_wizard_help'] = 'Натисніть на посилання, щоб запустити майстер установки.';
+$lang['devel_theme_viewer'] = 'тема перегляду';
+$lang['devel_theme_viewer_help'] = 'Натисніть на посилання, щоб переглянути тему.';
+$lang['devel_translations'] = 'переклади';
+$lang['devel_translation_mode'] = 'режим переведення';
+$lang['devel_view_theme'] = 'Перегляд Theme';
+$lang['devel_synchronize_updates'] = 'синхронізувати Оновлення';
+$lang['devel_synchronization_state_invalid'] = 'стан синхронізації є неприпустимим.';
