6e2c9657f8a71d1f4fa697e9b8969d2782ca1fc6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:12:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Developer Zouti';
+$lang['devel_app_description'] = 'Paj sa a bay yon pwomotè rapid BECA de tèm nan ak lòt widgets.';
+$lang['devel_install_wizard'] = 'enstale Sòsye';
+$lang['devel_install_wizard_help'] = 'Klike sou lyen ki anba la a yo kòmanse sòsye an enstale.';
+$lang['devel_theme_viewer'] = 'Tèm Viewer';
+$lang['devel_theme_viewer_help'] = 'Klike sou lyen ki anba la a yo revize tèm nan.';
+$lang['devel_translations'] = 'tradiksyon';
+$lang['devel_translation_mode'] = 'Mode tradiksyon';
+$lang['devel_view_theme'] = 'Gade Tèm';
+$lang['devel_synchronize_updates'] = 'Senkroniz Updates';
+$lang['devel_synchronization_state_invalid'] = 'Senkronizasyon Eta a se valab.';
