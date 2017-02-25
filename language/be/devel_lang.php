326af6a43819fb8f7742a73dbce3708e82fc18b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:09:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Інструменты для распрацоўшчыкаў';
+$lang['devel_app_description'] = 'На гэтай старонцы прадстаўлены кароткі агляд распрацоўшчык тэмы і іншых віджэтаў.';
+$lang['devel_install_wizard'] = 'майстар ўстаноўкі';
+$lang['devel_install_wizard_help'] = 'Націсніце на наступную спасылку, каб запусціць майстар ўстаноўкі.';
+$lang['devel_theme_viewer'] = 'тэма прагляду';
+$lang['devel_theme_viewer_help'] = 'Націсніце на наступную спасылку, каб прагледзець тэму.';
+$lang['devel_translations'] = 'пераклады';
+$lang['devel_translation_mode'] = 'рэжым перакладу';
+$lang['devel_view_theme'] = 'прагляд Theme';
+$lang['devel_synchronize_updates'] = 'сінхранізаваць Абнаўленні';
+$lang['devel_synchronization_state_invalid'] = 'стан сінхранізацыі з&#39;яўляецца недапушчальным.';
