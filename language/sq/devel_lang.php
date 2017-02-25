b05c50e8f8ab06351306bcd0888b87f2bf8353bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:08:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Developer Tools';
+$lang['devel_app_description'] = 'Kjo faqe ofron një pasqyrë të shpejtë zhvilluesi i temës dhe Widgets tjera.';
+$lang['devel_install_wizard'] = 'Install Wizard';
+$lang['devel_install_wizard_help'] = 'Klikoni mbi lidhjen e mëposhtme për të filluar të instaluar magjistar.';
+$lang['devel_theme_viewer'] = 'Theme Viewer';
+$lang['devel_theme_viewer_help'] = 'Klikoni mbi lidhjen e mëposhtme për të shqyrtuar temë.';
+$lang['devel_translations'] = 'përkthime';
+$lang['devel_translation_mode'] = 'Mode përkthim';
+$lang['devel_view_theme'] = 'Shiko Theme';
+$lang['devel_synchronize_updates'] = 'Synchronize Updates';
+$lang['devel_synchronization_state_invalid'] = 'Shteti sinkronizimi është i pavlefshëm.';
