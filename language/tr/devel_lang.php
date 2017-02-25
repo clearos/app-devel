d6452d4c9f8b893742d317c9cf2d4e0d295f9250
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:19:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Geliştirici Araçları';
+$lang['devel_app_description'] = 'Bu sayfa tema ve diğer aletler hızlı bir geliştirici bakış sağlar.';
+$lang['devel_install_wizard'] = 'Yükleme Sihirbazı';
+$lang['devel_install_wizard_help'] = 'kurulum sihirbazını başlatmak için aşağıdaki linke tıklayınız.';
+$lang['devel_theme_viewer'] = 'tema Görüntüleyici';
+$lang['devel_theme_viewer_help'] = 'tema gözden geçirmek için aşağıdaki linke tıklayınız.';
+$lang['devel_translations'] = 'Çeviriler';
+$lang['devel_translation_mode'] = 'çeviri Modu';
+$lang['devel_view_theme'] = 'Görünüm Tema';
+$lang['devel_synchronize_updates'] = 'Senkronize Güncellemeler';
+$lang['devel_synchronization_state_invalid'] = 'Senkronizasyon durumu geçersiz.';
