024ad8a3596b61bfd609a9259ca268191cce99c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:09:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'ادوات المطورين';
+$lang['devel_app_description'] = 'هذه الصفحة تقدم لمحة سريعة المطور للموضوع وغيرها من الحاجيات.';
+$lang['devel_install_wizard'] = 'تثبيت معالج';
+$lang['devel_install_wizard_help'] = 'انقر على الرابط التالي لبدء تثبيت المعالج.';
+$lang['devel_theme_viewer'] = 'عارض موضوع';
+$lang['devel_theme_viewer_help'] = 'انقر على الرابط التالي لمراجعة هذا الموضوع.';
+$lang['devel_translations'] = 'ترجمة';
+$lang['devel_translation_mode'] = 'وضع الترجمة';
+$lang['devel_view_theme'] = 'عرض الموضوع';
+$lang['devel_synchronize_updates'] = 'تزامن التحديثات';
+$lang['devel_synchronization_state_invalid'] = 'الدولة التزامن غير صالحة.';
