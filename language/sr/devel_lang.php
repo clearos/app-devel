18bdf7f01ec0ec430e16e3b07a91363b29142670
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:17:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Девелопер Тоолс';
+$lang['devel_app_description'] = 'Ова страница омогућава брз преглед за програмере на тему и других виџета.';
+$lang['devel_install_wizard'] = 'Инсталл Визард';
+$lang['devel_install_wizard_help'] = 'Кликните на следећи линк да бисте покренули инсталацију чаробњака.';
+$lang['devel_theme_viewer'] = 'Тема Виевер';
+$lang['devel_theme_viewer_help'] = 'Кликните на следећи линк да разматра тему.';
+$lang['devel_translations'] = 'преводи';
+$lang['devel_translation_mode'] = 'превод режим';
+$lang['devel_view_theme'] = 'Погледај Тема';
+$lang['devel_synchronize_updates'] = 'синцхронизе ажурирања';
+$lang['devel_synchronization_state_invalid'] = 'Синхронизација држава је неважећи.';
