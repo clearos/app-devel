7147c81632873a051a7926008967b91758b42402
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:16:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'विकासकर्ता उपकरण';
+$lang['devel_app_description'] = 'यो पृष्ठ विषय र अन्य विजेट छिटो विकासकर्ता सिंहावलोकन प्रदान गर्दछ।';
+$lang['devel_install_wizard'] = 'स्थापना विजार्ड';
+$lang['devel_install_wizard_help'] = 'को स्थापना विजार्ड सुरु गर्न निम्न लिंक मा क्लिक गर्नुहोस्।';
+$lang['devel_theme_viewer'] = 'विषय दर्शक';
+$lang['devel_theme_viewer_help'] = 'विषय समीक्षा गर्न निम्न लिंक मा क्लिक गर्नुहोस्।';
+$lang['devel_translations'] = 'अनुवाद';
+$lang['devel_translation_mode'] = 'अनुवाद मोड';
+$lang['devel_view_theme'] = 'विषयवस्तु हेर्नुहोस्';
+$lang['devel_synchronize_updates'] = 'सिंक्रोनाइज गर्नुहोस् अपडेट';
+$lang['devel_synchronization_state_invalid'] = 'समिकरण राज्य अमान्य छ।';
