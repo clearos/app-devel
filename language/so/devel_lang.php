021030c37674a6bfc5596b24769b588a15d281b4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:17:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Tools Developer';
+$lang['devel_app_description'] = 'Boggan waxaa ay bixinaysaa warbixin guud ka sameeysa deg deg ah oo ka mid ah theme iyo widgets kale.';
+$lang['devel_install_wizard'] = 'Ku rakib Wizard';
+$lang['devel_install_wizard_help'] = 'Guji link soo socda si ay u bilaabaan saaxir ah rakibi.';
+$lang['devel_theme_viewer'] = 'Daawade Theme';
+$lang['devel_theme_viewer_help'] = 'Guji link soo socda in dib loo eego mawduuca.';
+$lang['devel_translations'] = 'Tarjumidda';
+$lang['devel_translation_mode'] = 'Mode Translation';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'waafajinta Updates';
+$lang['devel_synchronization_state_invalid'] = 'gobolka .Wadashaqayntaas waa baadil.';
