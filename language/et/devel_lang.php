67129ae5534b8e2014b6e0afc65d13eab96ebb72
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:11:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Arendaja tööriistad';
+$lang['devel_app_description'] = 'See leht annab kiire arendaja ülevaate teema ja muud vidinad.';
+$lang['devel_install_wizard'] = 'Install Wizard';
+$lang['devel_install_wizard_help'] = 'Kliki sellele lingile alustada viisard.';
+$lang['devel_theme_viewer'] = 'Teema Viewer';
+$lang['devel_theme_viewer_help'] = 'Kliki sellele lingile vaadata teema.';
+$lang['devel_translations'] = 'Tõlked';
+$lang['devel_translation_mode'] = 'Tõlge režiim';
+$lang['devel_view_theme'] = 'Vaata Theme';
+$lang['devel_synchronize_updates'] = 'Sünkroniseeri uuendused';
+$lang['devel_synchronization_state_invalid'] = 'Sünkroniseerimine riik on kehtetu.';
