9e5c368fe4251a94b0846afca39ba642a6016028
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:16:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Utviklerverktøy';
+$lang['devel_app_description'] = 'Denne siden gir et raskt utvikler oversikt over temaet og andre widgets.';
+$lang['devel_install_wizard'] = 'Installer Wizard';
+$lang['devel_install_wizard_help'] = 'Klikk på følgende link for å starte installasjonsveiviseren.';
+$lang['devel_theme_viewer'] = 'Theme Viewer';
+$lang['devel_theme_viewer_help'] = 'Klikk på følgende link for å gjennomgå tema.';
+$lang['devel_translations'] = 'Oversettelser';
+$lang['devel_translation_mode'] = 'oversettelse Mode';
+$lang['devel_view_theme'] = 'Vis Theme';
+$lang['devel_synchronize_updates'] = 'Synkroniser oppdateringer';
+$lang['devel_synchronization_state_invalid'] = 'Synkronisering staten er ugyldig.';
