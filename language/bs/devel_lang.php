78d3cc1119ed96a86fd6e7363952447d09ed9b0b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:09:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Razvojni alati';
+$lang['devel_app_description'] = 'Ova stranica omogućava brz pregled developer teme i drugih widgeta.';
+$lang['devel_install_wizard'] = 'Install Wizard';
+$lang['devel_install_wizard_help'] = 'Kliknite na sljedeći link da biste pokrenuli instalaciju čarobnjak.';
+$lang['devel_theme_viewer'] = 'tema Viewer';
+$lang['devel_theme_viewer_help'] = 'Kliknite na sljedeći link za komentar na temu.';
+$lang['devel_translations'] = 'Prijevodi';
+$lang['devel_translation_mode'] = 'prevođenje Mode';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'Sinhronizacija Updates';
+$lang['devel_synchronization_state_invalid'] = 'Sinkronizacija država je nevažeća.';
