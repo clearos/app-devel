9ebd5071a4bd4d5e4a2452614cd39311b1f899bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:11:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Kehittäjän työkalut';
+$lang['devel_app_description'] = 'Tämä sivu tarjoaa nopean kehittäjä yleiskatsaus teema ja muut vekottimet.';
+$lang['devel_install_wizard'] = 'Asenna Wizard';
+$lang['devel_install_wizard_help'] = 'Klikkaa seuraavaa linkkiä käynnistääksesi ohjatun asennuksen.';
+$lang['devel_theme_viewer'] = 'teema Viewer';
+$lang['devel_theme_viewer_help'] = 'Klikkaa seuraavaa linkkiä tarkistaa teema.';
+$lang['devel_translations'] = 'Käännökset';
+$lang['devel_translation_mode'] = 'käännös tila';
+$lang['devel_view_theme'] = 'View Teema';
+$lang['devel_synchronize_updates'] = 'Synkronoi päivitykset';
+$lang['devel_synchronization_state_invalid'] = 'Synkronointi tila ei kelpaa.';
