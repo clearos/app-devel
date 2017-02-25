e4024ecd19eed449828cf3f1bb069abaf0e8d10a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:18:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Pakakas pamekar';
+$lang['devel_app_description'] = 'Kaca ieu nyadiakeun Tinjauan developer gancang tina tema jeung widget nu sejenna.';
+$lang['devel_install_wizard'] = 'masang Wizard';
+$lang['devel_install_wizard_help'] = 'Klik dina link di handap ieu pikeun ngamimitian masang wizard.';
+$lang['devel_theme_viewer'] = 'Viewer tema';
+$lang['devel_theme_viewer_help'] = 'Klik dina link di handap ieu pikeun nempoan téma.';
+$lang['devel_translations'] = 'Translations';
+$lang['devel_translation_mode'] = 'Mode Tarjamahan';
+$lang['devel_view_theme'] = 'Témbongkeun Téma';
+$lang['devel_synchronize_updates'] = 'singkronkeun Apdet';
+$lang['devel_synchronization_state_invalid'] = 'state sinkronisasi téh sah.';
