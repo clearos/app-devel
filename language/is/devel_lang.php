31bfac1a6779fdcfad680112ef0fe2ce8a935487
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:13:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Forritunarverkfæri';
+$lang['devel_app_description'] = 'Þessi síða afla a fljótur verktaki yfirlit yfir þemað og öðrum tækjum.';
+$lang['devel_install_wizard'] = 'setja Wizard';
+$lang['devel_install_wizard_help'] = 'Smelltu á eftirfarandi tengil til að hefja setja töframaður.';
+$lang['devel_theme_viewer'] = 'Þema Viewer';
+$lang['devel_theme_viewer_help'] = 'Smelltu á eftirfarandi tengil til að endurskoða þema.';
+$lang['devel_translations'] = 'Þýðingar';
+$lang['devel_translation_mode'] = 'þýðing Mode';
+$lang['devel_view_theme'] = 'Skoða Þema';
+$lang['devel_synchronize_updates'] = 'Samstilling uppfærslur';
+$lang['devel_synchronization_state_invalid'] = 'Samstilling ástand er ógilt.';
