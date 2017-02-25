3f5d4dbff457552a5586186ad23967897402b716
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:10:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Инструменти за разработчици';
+$lang['devel_app_description'] = 'Тази страница предоставя бърз преглед разработчик на темата и други джунджурии.';
+$lang['devel_install_wizard'] = 'Инсталирайте Wizard';
+$lang['devel_install_wizard_help'] = 'Кликнете върху следния линк, за да стартирате помощника за инсталиране.';
+$lang['devel_theme_viewer'] = 'Тема Viewer';
+$lang['devel_theme_viewer_help'] = 'Кликнете върху следния линк, за да направи преглед на темата.';
+$lang['devel_translations'] = 'Преводи';
+$lang['devel_translation_mode'] = 'Превод Mode';
+$lang['devel_view_theme'] = 'Виж Тема';
+$lang['devel_synchronize_updates'] = 'Синхронизирайте Updates';
+$lang['devel_synchronization_state_invalid'] = 'Синхронизация състояние е невалиден.';
