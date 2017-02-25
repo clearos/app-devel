b938499e076204a82f9f395d24fdaa08bf0b70b8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:11:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Εργαλεία προγραμματιστή';
+$lang['devel_app_description'] = 'Αυτή η σελίδα παρέχει μια γρήγορη επισκόπηση του έργου του θέματος και άλλες μικροσυσκευές.';
+$lang['devel_install_wizard'] = 'Οδηγός εγκατάστασης';
+$lang['devel_install_wizard_help'] = 'Κάντε κλικ στον παρακάτω σύνδεσμο για να ξεκινήσει ο οδηγός εγκατάστασης.';
+$lang['devel_theme_viewer'] = 'θέμα Viewer';
+$lang['devel_theme_viewer_help'] = 'Κάντε κλικ στον παρακάτω σύνδεσμο για να επανεξετάσει το θέμα.';
+$lang['devel_translations'] = 'Μεταφράσεις';
+$lang['devel_translation_mode'] = 'μετάφραση Λειτουργία';
+$lang['devel_view_theme'] = 'Προβολή θέματος';
+$lang['devel_synchronize_updates'] = 'Συγχρονισμός Ενημερώσεις';
+$lang['devel_synchronization_state_invalid'] = 'κατάσταση συγχρονισμού δεν είναι έγκυρη.';
