8feeea8b697c2729f97d25345c47ee3e48089850
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:20:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'דעוועלאָפּער מכשירים';
+$lang['devel_app_description'] = 'דעם בלאַט גיט אַ שנעל דעוועלאָפּער איבערבליק פון די טעמע און אנדערע ווידזשיץ.';
+$lang['devel_install_wizard'] = 'ינסטאַלירן וויזערד';
+$lang['devel_install_wizard_help'] = 'דריקט אויף די ווייַטערדיק לינק צו אָנהייבן די ינסטאַלירן מאַזעק.';
+$lang['devel_theme_viewer'] = 'טים וויוער';
+$lang['devel_theme_viewer_help'] = 'דריקט אויף די ווייַטערדיק לינק צו אָפּשאַצונג די טעמע.';
+$lang['devel_translations'] = 'איבערזעצונגען';
+$lang['devel_translation_mode'] = 'איבערזעצונג מאָדע';
+$lang['devel_view_theme'] = 'קוק טים';
+$lang['devel_synchronize_updates'] = 'סינטשראָניזע ופּדאַטעס';
+$lang['devel_synchronization_state_invalid'] = 'סינטשראָניזאַטיאָן שטאַט איז פאַרקריפּלט.';
