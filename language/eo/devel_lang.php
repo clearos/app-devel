87cd20b3fdf25e504d0e496dea528bc25445201a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:11:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'programisto iloj';
+$lang['devel_app_description'] = 'Ĉi tiu paĝo provizas rapidan ellaboranto superrigardon de la temo kaj aliaj widgets.';
+$lang['devel_install_wizard'] = 'instali Sorĉisto';
+$lang['devel_install_wizard_help'] = 'Alklaku la sekvan ligilon por komenci la instalon sorĉisto.';
+$lang['devel_theme_viewer'] = 'temo rigardiloName';
+$lang['devel_theme_viewer_help'] = 'Alklaku la sekvan ligilon por revizii la temo.';
+$lang['devel_translations'] = 'tradukoj';
+$lang['devel_translation_mode'] = 'translation Mode';
+$lang['devel_view_theme'] = 'view Temo';
+$lang['devel_synchronize_updates'] = 'Sinkronigu ĝisdatigoj';
+$lang['devel_synchronization_state_invalid'] = 'Sincronización stato estas nevalida.';
