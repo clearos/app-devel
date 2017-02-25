cbaea127ac46a2c2ae16320b67973fdc433d677a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:14:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Developer Tools';
+$lang['devel_app_description'] = 'Ity pejy ity dia manome haingana developer ambangovangony ny foto-kevitra sy ny gadget hafa.';
+$lang['devel_install_wizard'] = 'hametraka Le Magicien';
+$lang['devel_install_wizard_help'] = 'Tsindrio ny rohy manaraka mba hanomboka ny hametraka Magicien.';
+$lang['devel_theme_viewer'] = 'Theme Viewer';
+$lang['devel_theme_viewer_help'] = 'Tsindrio ny rohy manaraka ity mba hamerina hijery ny foto-kevitra.';
+$lang['devel_translations'] = 'Anarana iombonana';
+$lang['devel_translation_mode'] = 'Translation Mode';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'Synchronize Updates';
+$lang['devel_synchronization_state_invalid'] = 'Synchronization fanjakana dia tsy manan-kery.';
