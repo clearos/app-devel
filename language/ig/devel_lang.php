ad056da4a5b1ecf19649eb6fa7f4be3c4225c23f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:13:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Mmepụta Tools';
+$lang['devel_app_description'] = 'A na peeji nke na-enye a ngwa Mmepụta nnyocha nke isiokwu ndị ọzọ wijetị.';
+$lang['devel_install_wizard'] = 'wụnye Wizard';
+$lang['devel_install_wizard_help'] = 'Pịa na-esonụ njikọ na-amalite nwụnye ọkachamara.';
+$lang['devel_theme_viewer'] = 'isiokwu onye nkiri';
+$lang['devel_theme_viewer_help'] = 'Pịa na-esonụ njikọ iji nyochaa isiokwu.';
+$lang['devel_translations'] = 'nsụgharị';
+$lang['devel_translation_mode'] = 'Translation mode';
+$lang['devel_view_theme'] = 'View Okwu';
+$lang['devel_synchronize_updates'] = 'mekọrịta Updates';
+$lang['devel_synchronization_state_invalid'] = 'Ngakọrịta ala bụ ghara ịdị irè.';
