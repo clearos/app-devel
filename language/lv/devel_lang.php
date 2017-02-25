2a67aa1cda51e3a9d0b1fb4f9dd10378f08025dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:14:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Izstrādātāju rīki';
+$lang['devel_app_description'] = 'Šī lapa nodrošina ātru attīstītājs pārskatu par tēmu un citiem widgets.';
+$lang['devel_install_wizard'] = 'Install Wizard';
+$lang['devel_install_wizard_help'] = 'Uzklikšķiniet uz šīs saites, lai sāktu instalēt vedni.';
+$lang['devel_theme_viewer'] = 'tēma Viewer';
+$lang['devel_theme_viewer_help'] = 'Uzklikšķiniet uz šīs saites, lai pārskatītu tēmu.';
+$lang['devel_translations'] = 'Tulkojumi';
+$lang['devel_translation_mode'] = 'tulkošanas Mode';
+$lang['devel_view_theme'] = 'Skatīt Theme';
+$lang['devel_synchronize_updates'] = 'Sinhronizēt Updates';
+$lang['devel_synchronization_state_invalid'] = 'Sinhronizācija stāvoklis ir nederīgs.';
