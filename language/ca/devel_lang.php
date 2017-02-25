90f5d2fce6a329537a4a0d5e2df3f160c7c08bf4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:10:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Eines de Desenvolupament';
+$lang['devel_app_description'] = 'Aquesta pàgina ofereix una visió general ràpida desenvolupador del tema i altres ginys.';
+$lang['devel_install_wizard'] = 'Assistent d&#39;instal·lació';
+$lang['devel_install_wizard_help'] = 'Feu clic al següent enllaç per iniciar l&#39;assistent d&#39;instal·lació.';
+$lang['devel_theme_viewer'] = 'visor tema';
+$lang['devel_theme_viewer_help'] = 'Feu clic al següent enllaç per revisar el tema.';
+$lang['devel_translations'] = 'traduccions';
+$lang['devel_translation_mode'] = 'Mode de traducció';
+$lang['devel_view_theme'] = 'Mostra el tema';
+$lang['devel_synchronize_updates'] = 'sincronitzar actualitzacions';
+$lang['devel_synchronization_state_invalid'] = 'estat de sincronització no és vàlid.';
