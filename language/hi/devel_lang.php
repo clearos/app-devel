f91ae23d9d7ce498cc5a73a4142fefe91f6264cf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:12:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'डेवलपर उपकरण';
+$lang['devel_app_description'] = 'यह पृष्ठ विषय और अन्य विजेट की एक त्वरित डेवलपर अवलोकन प्रदान करता है।';
+$lang['devel_install_wizard'] = 'जादूगर स्थापित करें';
+$lang['devel_install_wizard_help'] = 'नीचे दिए गए लिंक पर क्लिक करें विज़ार्ड स्थापित शुरू करने के लिए।';
+$lang['devel_theme_viewer'] = 'विषय दर्शक';
+$lang['devel_theme_viewer_help'] = 'नीचे दिए गए लिंक पर क्लिक करें विषय की समीक्षा करने के लिए।';
+$lang['devel_translations'] = 'में अनुवाद';
+$lang['devel_translation_mode'] = 'अनुवाद मोड';
+$lang['devel_view_theme'] = 'थीम देखें';
+$lang['devel_synchronize_updates'] = 'सिंक्रनाइज़ अपडेट';
+$lang['devel_synchronization_state_invalid'] = 'तुल्यकालन राज्य अमान्य है।';
