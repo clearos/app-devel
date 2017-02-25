be5c04095fd927f868d53c4fa93f46e77679043a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:17:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Instrumente de dezvoltare';
+$lang['devel_app_description'] = 'Această pagină oferă o privire de ansamblu dezvoltator rapidă a temei și a altor widget-uri.';
+$lang['devel_install_wizard'] = 'Install Wizard';
+$lang['devel_install_wizard_help'] = 'Faceți clic pe link-ul de mai jos pentru a porni expertul de instalare.';
+$lang['devel_theme_viewer'] = 'tema Viewer';
+$lang['devel_theme_viewer_help'] = 'Faceți clic pe link-ul de mai jos pentru a examina tema.';
+$lang['devel_translations'] = 'traduceri';
+$lang['devel_translation_mode'] = 'Mod traducere';
+$lang['devel_view_theme'] = 'Vizualizare Tema';
+$lang['devel_synchronize_updates'] = 'Synchronize Actualizări';
+$lang['devel_synchronization_state_invalid'] = 'starea de sincronizare este nevalid.';
