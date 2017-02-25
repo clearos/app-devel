a7c3786e50af37a491bebb86115e3185c5c9b37a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:20:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'developer Tools';
+$lang['devel_app_description'] = 'Iwe yi pese a ọna developer Akopọ ti awọn akori ati awọn miiran ẹrọ ailorukọ.';
+$lang['devel_install_wizard'] = 'fi sori ẹrọ oso';
+$lang['devel_install_wizard_help'] = 'Tẹ lori awọn wọnyi ọna asopọ lati bẹrẹ awọn sori ẹrọ oluṣeto.';
+$lang['devel_theme_viewer'] = 'akori wiwo';
+$lang['devel_theme_viewer_help'] = 'Tẹ lori awọn wọnyi ọna asopọ lati ṣe ayẹwo awọn akori.';
+$lang['devel_translations'] = 'ìtumọ';
+$lang['devel_translation_mode'] = 'Translation mode';
+$lang['devel_view_theme'] = 'Wo Akori';
+$lang['devel_synchronize_updates'] = 'Muu awọn imudojuiwọn';
+$lang['devel_synchronization_state_invalid'] = 'Amuṣiṣẹpọ ipinle jẹ invalid.';
