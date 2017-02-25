30d2b1eb82ae4abca94c75bc2e6bc96daa987648
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:19:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'เครื่องมือสำหรับนักพัฒนา';
+$lang['devel_app_description'] = 'หน้านี้ให้ภาพรวมนักพัฒนาอย่างรวดเร็วของรูปแบบและเครื่องมืออื่น ๆ';
+$lang['devel_install_wizard'] = 'ตัวช่วยสร้างการติดตั้ง';
+$lang['devel_install_wizard_help'] = 'คลิกที่ลิงค์ต่อไปนี้เพื่อเริ่มต้นการติดตั้งตัวช่วยสร้าง';
+$lang['devel_theme_viewer'] = 'Viewer ธีม';
+$lang['devel_theme_viewer_help'] = 'คลิกที่ลิงค์ต่อไปนี้เพื่อตรวจสอบรูปแบบ';
+$lang['devel_translations'] = 'การแปล';
+$lang['devel_translation_mode'] = 'โหมดการแปล';
+$lang['devel_view_theme'] = 'ดูธีม';
+$lang['devel_synchronize_updates'] = 'อัพเดทข้อมูลให้ตรงกัน';
+$lang['devel_synchronization_state_invalid'] = 'รัฐประสานข้อมูลไม่ถูกต้อง';
