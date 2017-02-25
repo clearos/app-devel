f7d6dd6cc0837ecd8a898c77b94d7b38521ca395
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:15:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'ഡെവലപ്പർ ഉപകരണങ്ങൾ';
+$lang['devel_app_description'] = 'ഈ താൾ തീം മറ്റ് വിഡ്ജറ്റുകൾ ഒരു ദ്രുത ഡെവലപ്പർ നിരത്തുന്നു.';
+$lang['devel_install_wizard'] = 'ഇൻസ്റ്റാൾ വിസാർഡ്';
+$lang['devel_install_wizard_help'] = 'ഇൻസ്റ്റാൾ വിസാർഡ് ആരംഭിക്കാൻ ഇനിപ്പറയുന്ന ലിങ്കിൽ ക്ലിക്ക് ചെയ്യുക.';
+$lang['devel_theme_viewer'] = 'തീം വ്യൂവർ';
+$lang['devel_theme_viewer_help'] = 'തീം അവലോകനം ഇനിപ്പറയുന്ന ലിങ്കിൽ ക്ലിക്ക് ചെയ്യുക.';
+$lang['devel_translations'] = 'പരിഭാഷകൾ';
+$lang['devel_translation_mode'] = 'പരിഭാഷ മോഡ്';
+$lang['devel_view_theme'] = 'കാണുക തീം';
+$lang['devel_synchronize_updates'] = 'അപ്ഡേറ്റുകൾ സമന്വയിപ്പിക്കുക';
+$lang['devel_synchronization_state_invalid'] = 'സമന്വയ സംസ്ഥാന അസാധുവാണ്.';
