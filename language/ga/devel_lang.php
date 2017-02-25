7d4574d918e02c9a3318b6eb85b30cd58734b0cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:12:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Uirlisí d&#39;Fhorbróirí';
+$lang['devel_app_description'] = 'Cuireann an leathanach seo forbhreathnú forbróir tapa ar an téama agus giuirléidí eile.';
+$lang['devel_install_wizard'] = 'Suiteáil Treoraí';
+$lang['devel_install_wizard_help'] = 'Cliceáil ar an nasc seo a leanas chun tús a chur leis an draoi a shuiteáil.';
+$lang['devel_theme_viewer'] = 'Amharcán téama';
+$lang['devel_theme_viewer_help'] = 'Cliceáil ar an nasc seo a leanas chun athbhreithniú a dhéanamh téama.';
+$lang['devel_translations'] = 'Aistriúcháin';
+$lang['devel_translation_mode'] = 'Mód aistriúchán';
+$lang['devel_view_theme'] = 'Féach ar Téama';
+$lang['devel_synchronize_updates'] = 'Sioncrónaigh Nuashonruithe';
+$lang['devel_synchronization_state_invalid'] = 'Is Synchronization stáit neamhbhailí.';
