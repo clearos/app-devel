31768e37ba4a97cd602fb563140a05c2ec8b2e55
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:16:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Developer tools';
+$lang['devel_app_description'] = 'Deze pagina biedt een snelle ontwikkelaar overzicht van het thema en andere widgets.';
+$lang['devel_install_wizard'] = 'Install Wizard';
+$lang['devel_install_wizard_help'] = 'Klik op de volgende link om de installatiewizard te starten.';
+$lang['devel_theme_viewer'] = 'Theme Viewer';
+$lang['devel_theme_viewer_help'] = 'Klik op de volgende link om het thema te herzien.';
+$lang['devel_translations'] = 'vertaalwerk';
+$lang['devel_translation_mode'] = 'vertaling Mode';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'synchroniseren updates';
+$lang['devel_synchronization_state_invalid'] = 'Synchronisatie staat is ongeldig.';
