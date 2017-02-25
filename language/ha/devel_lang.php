12d5cafcbe67dca7e13228af49911e12ea30367f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:12:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'developer Tools';
+$lang['devel_app_description'] = 'Wannan shafin samar da wani m developer bayyani na theme da kuma sauran Widgets.';
+$lang['devel_install_wizard'] = 'shigar Wizard';
+$lang['devel_install_wizard_help'] = 'Click a kan wadannan link don fara da shigar maye.';
+$lang['devel_theme_viewer'] = 'theme Vidiyo';
+$lang['devel_theme_viewer_help'] = 'Click a kan wadannan link to bitar theme.';
+$lang['devel_translations'] = 'Fassarori';
+$lang['devel_translation_mode'] = 'translation Mode';
+$lang['devel_view_theme'] = 'view Theme';
+$lang['devel_synchronize_updates'] = 'Yi aiki tare akan Updates';
+$lang['devel_synchronization_state_invalid'] = 'Aiki tare jihar ba daidai ba ne.';
