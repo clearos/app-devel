f19014492c677a65f0e97fbdc1e23aea92e78d9d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:12:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Fejlesztői eszközök';
+$lang['devel_app_description'] = 'Ez az oldal egy gyors fejlesztő áttekintést a téma és a többi kütyü.';
+$lang['devel_install_wizard'] = 'Install Wizard';
+$lang['devel_install_wizard_help'] = 'Kattintson az alábbi linkre, hogy indítsa el a telepítő varázsló.';
+$lang['devel_theme_viewer'] = 'Témamegjelenítőjével';
+$lang['devel_theme_viewer_help'] = 'Kattintson az alábbi linkre, hogy vizsgálja felül a témát.';
+$lang['devel_translations'] = 'Fordítások';
+$lang['devel_translation_mode'] = 'Translation Mode';
+$lang['devel_view_theme'] = 'Részletek téma';
+$lang['devel_synchronize_updates'] = 'Szinkronizálás frissítések';
+$lang['devel_synchronization_state_invalid'] = 'A szinkronizálás állapota nem megfelelő.';
