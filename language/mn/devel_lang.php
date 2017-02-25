947b0d9d9956f62b3073dd295b407294326cbc6b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:15:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Developer Tools';
+$lang['devel_app_description'] = 'Энэ хуудсанд сэдэв болон бусад багажнуудын хурдан хөгжүүлэгч тоймлон үзүүлдэг.';
+$lang['devel_install_wizard'] = 'Wizard суулгах';
+$lang['devel_install_wizard_help'] = 'суулгах шидтэн эхлэх нь дараах холбоос дээр дарна уу.';
+$lang['devel_theme_viewer'] = 'Сэдэв Viewer';
+$lang['devel_theme_viewer_help'] = 'сэдвийг хянаж дараах холбоос дээр дарна уу.';
+$lang['devel_translations'] = 'орчуулга';
+$lang['devel_translation_mode'] = 'орчуулга горим';
+$lang['devel_view_theme'] = 'Сэдэв харах';
+$lang['devel_synchronize_updates'] = 'хамгийн сүүлийн хэлбэрт аваачиж Шинэчлэлтүүд нь';
+$lang['devel_synchronization_state_invalid'] = 'Тааруулах нь төрийн хүчин төгөлдөр бус байна.';
