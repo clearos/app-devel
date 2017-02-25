11d5235bdadde97fd0291dd2223c4ed8cae6eb62
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:10:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Vývojářské nástroje';
+$lang['devel_app_description'] = 'Tato stránka poskytuje rychlý přehled vývojář tématu a další widgety.';
+$lang['devel_install_wizard'] = 'Průvodce instalací';
+$lang['devel_install_wizard_help'] = 'Kliknout na následující odkaz pro spuštění Průvodce instalací.';
+$lang['devel_theme_viewer'] = 'téma Viewer';
+$lang['devel_theme_viewer_help'] = 'Kliknout na následující odkaz, aby přezkoumala téma.';
+$lang['devel_translations'] = 'Překlady';
+$lang['devel_translation_mode'] = 'Režim překlad';
+$lang['devel_view_theme'] = 'Zobrazit téma';
+$lang['devel_synchronize_updates'] = 'Synchronizace Aktualizace';
+$lang['devel_synchronization_state_invalid'] = 'Synchronizace stav je neplatný.';
