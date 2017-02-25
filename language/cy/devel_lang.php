bf912e8fb2e8c3cb20ba4b42e4cfc59a86d2032e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:20:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Offer datblygwr';
+$lang['devel_app_description'] = 'Mae&#39;r dudalen hon yn darparu trosolwg datblygwr cyflym o&#39;r thema a widgets eraill.';
+$lang['devel_install_wizard'] = 'Gosod Dewin';
+$lang['devel_install_wizard_help'] = 'Cliciwch ar y ddolen ganlynol i ddechrau &#39;r gorsedda brudiwr.';
+$lang['devel_theme_viewer'] = 'Gwyliwr thema';
+$lang['devel_theme_viewer_help'] = 'Cliciwch ar y ddolen ganlynol i adolygu&#39;r thema.';
+$lang['devel_translations'] = 'Cyfieithiadau';
+$lang['devel_translation_mode'] = 'Modd cyfieithu';
+$lang['devel_view_theme'] = 'Gweld Thema';
+$lang['devel_synchronize_updates'] = 'Cydamseru Diweddariadau';
+$lang['devel_synchronization_state_invalid'] = 'Synchronization wladwriaeth yn annilys.';
