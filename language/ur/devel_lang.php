ae1dd2fc7d4e15f309fa3584d861565e923d1043
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:19:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'ڈیولپر کے اوزار';
+$lang['devel_app_description'] = 'اس صفحہ تھیم اور دوسرے ویجٹ کی ایک فوری جائزہ فراہم ڈیولپر.';
+$lang['devel_install_wizard'] = 'انسٹال مددگار';
+$lang['devel_install_wizard_help'] = 'انسٹال مددگار شروع کرنے کے لئے مندرجہ ذیل لنک پر کلک کریں.';
+$lang['devel_theme_viewer'] = 'موضوع ناظر';
+$lang['devel_theme_viewer_help'] = 'تھیم جائزہ لینے کے لئے مندرجہ ذیل لنک پر کلک کریں.';
+$lang['devel_translations'] = 'ترجمے';
+$lang['devel_translation_mode'] = 'ترجمہ کے موڈ';
+$lang['devel_view_theme'] = 'لنک تھیم';
+$lang['devel_synchronize_updates'] = 'ہم وقت کی تازہ ترین معلومات';
+$lang['devel_synchronization_state_invalid'] = 'ہم وقت سازی ریاست باطل ہے.';
