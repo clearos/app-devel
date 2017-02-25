e2f85ea0a422e06e2ad150577badc609e2c87e79
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:12:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'વિકાસકર્તા સાધનો';
+$lang['devel_app_description'] = 'આ પાનું થીમ અને અન્ય વિજેટો એક ઝડપી ડેવલપર ઝાંખી પૂરી પાડે છે.';
+$lang['devel_install_wizard'] = 'વિઝાર્ડ સ્થાપિત';
+$lang['devel_install_wizard_help'] = 'નીચેની લિંક પર ક્લિક કરો વિઝાર્ડ સ્થાપિત શરૂ કરવા માટે.';
+$lang['devel_theme_viewer'] = 'થીમ દર્શક';
+$lang['devel_theme_viewer_help'] = 'નીચેની લિંક પર ક્લિક કરો થીમ સમીક્ષા કરવા માટે.';
+$lang['devel_translations'] = 'માં ભાષાંતરો';
+$lang['devel_translation_mode'] = 'અનુવાદ સ્થિતિ';
+$lang['devel_view_theme'] = 'થીમ જોવા';
+$lang['devel_synchronize_updates'] = 'સમન્વય સુધારાઓ';
+$lang['devel_synchronization_state_invalid'] = 'સુમેળ રાજ્ય અમાન્ય છે.';
