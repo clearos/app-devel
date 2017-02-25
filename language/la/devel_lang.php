5a798183b7a058c5c22d82fd30342eea20c0a5bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:14:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'elit Tools';
+$lang['devel_app_description'] = 'Haec pagina dat a elit vivos et widgets overview of lemma.';
+$lang['devel_install_wizard'] = 'Install Veneficus';
+$lang['devel_install_wizard_help'] = 'Click consequat mauris install veneficus.';
+$lang['devel_theme_viewer'] = 'Theme Results';
+$lang['devel_theme_viewer_help'] = 'Click recognoscere consequat elit.';
+$lang['devel_translations'] = 'Translations';
+$lang['devel_translation_mode'] = 'Modus Translation';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'Synchronize Updates';
+$lang['devel_synchronization_state_invalid'] = 'Synchronization statu est invalidum.';
