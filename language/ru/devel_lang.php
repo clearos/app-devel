99f181049b7e186a61e870f0ab2ad6d6e3c833fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:17:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Инструменты разработчика';
+$lang['devel_app_description'] = 'На этой странице представлен краткий обзор разработчик темы и других виджетов.';
+$lang['devel_install_wizard'] = 'Мастер установки';
+$lang['devel_install_wizard_help'] = 'Нажмите на следующую ссылку, чтобы запустить мастер установки.';
+$lang['devel_theme_viewer'] = 'тема просмотра';
+$lang['devel_theme_viewer_help'] = 'Нажмите на следующую ссылку, чтобы просмотреть тему.';
+$lang['devel_translations'] = 'Переводы';
+$lang['devel_translation_mode'] = 'Режим перевода';
+$lang['devel_view_theme'] = 'Просмотр Theme';
+$lang['devel_synchronize_updates'] = 'Синхронизировать Обновления';
+$lang['devel_synchronization_state_invalid'] = 'состояние синхронизации является недопустимым.';
