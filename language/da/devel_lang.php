1cebf40fbad82019c85757f8c3470a73d6cc9d8d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:11:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Developer Tools';
+$lang['devel_app_description'] = 'Denne side giver et hurtigt udvikler overblik over temaet og andre widgets.';
+$lang['devel_install_wizard'] = 'Installer Wizard';
+$lang['devel_install_wizard_help'] = 'Klik på følgende link for at starte installationsguiden.';
+$lang['devel_theme_viewer'] = 'Tema Viewer';
+$lang['devel_theme_viewer_help'] = 'Klik på følgende link for at gennemgå temaet.';
+$lang['devel_translations'] = 'Oversættelser';
+$lang['devel_translation_mode'] = 'Oversættelse tilstand';
+$lang['devel_view_theme'] = 'Vis Tema';
+$lang['devel_synchronize_updates'] = 'Synkroniser opdateringer';
+$lang['devel_synchronization_state_invalid'] = 'Synkronisering tilstand er ugyldig.';
