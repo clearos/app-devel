782af412475d0a6285ecc18d007180cb551db1e2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:12:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Alat pengembang';
+$lang['devel_app_description'] = 'Halaman ini memberikan gambaran pengembang cepat tema dan widget lainnya.';
+$lang['devel_install_wizard'] = 'install wizard';
+$lang['devel_install_wizard_help'] = 'Klik pada link berikut untuk memulai instalasi Wizard.';
+$lang['devel_theme_viewer'] = 'Viewer tema';
+$lang['devel_theme_viewer_help'] = 'Klik pada link berikut untuk meninjau tema.';
+$lang['devel_translations'] = 'terjemahan';
+$lang['devel_translation_mode'] = 'Modus terjemahan';
+$lang['devel_view_theme'] = 'Lihat Tema';
+$lang['devel_synchronize_updates'] = 'sinkronisasi Updates';
+$lang['devel_synchronization_state_invalid'] = 'negara sinkronisasi tidak valid.';
