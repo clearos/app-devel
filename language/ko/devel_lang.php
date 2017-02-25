52e2ea1c67a232e9aa14e44bf8dda2fef097eb7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:14:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = '개발자 도구';
+$lang['devel_app_description'] = '이 페이지는 주제와 다른 위젯의 빠른 개발의 개요를 제공합니다.';
+$lang['devel_install_wizard'] = '설치 마법사';
+$lang['devel_install_wizard_help'] = '설치 마법사를 시작하려면 다음 링크를 클릭하십시오.';
+$lang['devel_theme_viewer'] = '테마 뷰어';
+$lang['devel_theme_viewer_help'] = '테마를 검토하려면 다음 링크를 클릭하십시오.';
+$lang['devel_translations'] = '번역';
+$lang['devel_translation_mode'] = '번역 모드';
+$lang['devel_view_theme'] = '보기 테마';
+$lang['devel_synchronize_updates'] = '동기화 업데이트';
+$lang['devel_synchronization_state_invalid'] = '동기화 상태가 잘못되었습니다.';
