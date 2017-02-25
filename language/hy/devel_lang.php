ef3a4877d3e1dc9ac4fd976cf438c1a58441ff7d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:09:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Մշակողի գործիքներ';
+$lang['devel_app_description'] = 'Սա էջի ապահովում է արագ մշակողի ակնարկ թեմայով եւ այլ ֆայլեր.';
+$lang['devel_install_wizard'] = 'Տեղադրեք Wizard';
+$lang['devel_install_wizard_help'] = 'Սեղմեք հետեւյալ հղումը սկսել տեղադրել հրաշագործ.';
+$lang['devel_theme_viewer'] = 'Theme հեռուստադիտողի';
+$lang['devel_theme_viewer_help'] = 'Սեղմեք հետեւյալ հղումը վերանայել թեման:';
+$lang['devel_translations'] = 'Թարգմանություններ';
+$lang['devel_translation_mode'] = 'Translation Mode';
+$lang['devel_view_theme'] = 'Դիտել Theme';
+$lang['devel_synchronize_updates'] = 'Սինքրոնացրեք թարմացումներ';
+$lang['devel_synchronization_state_invalid'] = 'Համաժամացման պետությունն է անվավեր:';
