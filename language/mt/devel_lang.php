10bfa6a2718062a5218efddb99736f63ba07b015
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:15:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Developer Tools';
+$lang['devel_app_description'] = 'Din il-paġna tipprovdi ħarsa ġenerali żviluppatur ta &#39;malajr tat-tema u oħrajn widgets.';
+$lang['devel_install_wizard'] = 'Installa Wizard';
+$lang['devel_install_wizard_help'] = 'Ikklikkja fuq il-link li ġejja biex jibdew il-jinstallaw Wizard.';
+$lang['devel_theme_viewer'] = 'Viewer tema';
+$lang['devel_theme_viewer_help'] = 'Ikklikkja fuq il-link li ġejja biex tirrevedi l-tema.';
+$lang['devel_translations'] = 'traduzzjonijiet';
+$lang['devel_translation_mode'] = 'Modalità traduzzjoni';
+$lang['devel_view_theme'] = 'View Tema';
+$lang['devel_synchronize_updates'] = 'Synchronize Aġġornamenti';
+$lang['devel_synchronization_state_invalid'] = 'istat Sinkronizzazzjoni huwa invalidu.';
