459199d53f7beeef290f3903eba455f64ccac645
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:19:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Tuzuvchi Vositalari';
+$lang['devel_app_description'] = 'Bu sahifa mavzusida va boshqa vidjet tez developer umumiy beradi.';
+$lang['devel_install_wizard'] = 'Wizard o&#39;rnatish';
+$lang['devel_install_wizard_help'] = 'o&#39;rnatish ustasini boshlash uchun quyidagi linkni bosing.';
+$lang['devel_theme_viewer'] = 'Theme Viewer';
+$lang['devel_theme_viewer_help'] = 'Mavzuni ko&#39;rib uchun quyidagi linkni bosing.';
+$lang['devel_translations'] = 'Tarjimalar';
+$lang['devel_translation_mode'] = 'Tarjima Mode';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'Sinxronizatsiya yangilash';
+$lang['devel_synchronization_state_invalid'] = 'Sinxronlashtirish davlat haqiqiy emas.';
