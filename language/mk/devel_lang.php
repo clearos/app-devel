4ab57d42ac5f2317d31fcfc179f118c8ad246f2c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:15:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Алатки за програмери';
+$lang['devel_app_description'] = 'Оваа страница дава брз преглед инвеститорот на темата и други додатоци.';
+$lang['devel_install_wizard'] = 'инсталацијата волшебникот';
+$lang['devel_install_wizard_help'] = 'Кликнете на следниот линк за да го стартувате волшебникот за инсталирање.';
+$lang['devel_theme_viewer'] = 'тема Прегледувач';
+$lang['devel_theme_viewer_help'] = 'Кликнете на следниот линк за да ги видиш тема.';
+$lang['devel_translations'] = 'Превод';
+$lang['devel_translation_mode'] = 'превод режим';
+$lang['devel_view_theme'] = 'Види Тема';
+$lang['devel_synchronize_updates'] = 'Синхронизирате Новости';
+$lang['devel_synchronization_state_invalid'] = 'Синхронизација на државата не е валиден.';
