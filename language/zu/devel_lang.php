3f78d94523f8678991e9e92cc2340d179f385798
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:20:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Developer Tools';
+$lang['devel_app_description'] = 'Leli khasi inikeza umqambi umbono esheshayo isihloko kanye nezinye amawijethi.';
+$lang['devel_install_wizard'] = 'Faka Wizard';
+$lang['devel_install_wizard_help'] = 'Chofoza isixhumanisi elandelayo ukuqala ukufaka wizadi.';
+$lang['devel_theme_viewer'] = 'Theme Viewer';
+$lang['devel_theme_viewer_help'] = 'Chofoza isixhumanisi esilandelayo ukuze ubuyekeze indaba ephathwayo.';
+$lang['devel_translations'] = 'Translations';
+$lang['devel_translation_mode'] = 'Mode Translation';
+$lang['devel_view_theme'] = 'Buka Theme';
+$lang['devel_synchronize_updates'] = 'Vumelanisa Updates';
+$lang['devel_synchronization_state_invalid'] = 'isimo Ukuvumelanisa ayivumelekile.';
