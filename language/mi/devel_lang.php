b13f78777e9895c1303033c338c5d4d4242e218f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:15:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'Utauta Developer';
+$lang['devel_app_description'] = 'E whakarato ana tēnei whārangi i te tirohanga whānui kaiwhakawhanake tere o te kaupapa, me te tahi atu widget.';
+$lang['devel_install_wizard'] = 'tāuta Ruānuku';
+$lang['devel_install_wizard_help'] = 'Pāwhiritia i runga i te hononga e whai ake ki te tīmata i te tāuta te ruānuku.';
+$lang['devel_theme_viewer'] = 'Viewer kaupapa';
+$lang['devel_theme_viewer_help'] = 'Pāwhiritia i runga i te hononga e whai ake ki te arotake i te kaupapa.';
+$lang['devel_translations'] = 'Translations';
+$lang['devel_translation_mode'] = 'Aratau Translation';
+$lang['devel_view_theme'] = 'Tirohia Kaupapa';
+$lang['devel_synchronize_updates'] = 'Tukutahi Updates';
+$lang['devel_synchronization_state_invalid'] = 'He muhu āhua tukutahitanga.';
