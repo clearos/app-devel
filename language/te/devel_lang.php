443d12b3e22b125e5d0869b996491300ba37d7c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:18:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'డెవలపర్ ఉపకరణాలు';
+$lang['devel_app_description'] = 'ఈ పేజీని థీమ్ మరియు ఇతర విడ్జెట్ యొక్క శీఘ్ర డెవలపర్ అవలోకనాన్ని అందిస్తుంది.';
+$lang['devel_install_wizard'] = 'ఇన్స్టాల్ విజార్డ్';
+$lang['devel_install_wizard_help'] = 'ఇన్స్టాల్ విజర్డ్ ప్రారంభించేందుకు క్రింది లింక్పై క్లిక్ చేయండి.';
+$lang['devel_theme_viewer'] = 'థీమ్ వ్యూయర్';
+$lang['devel_theme_viewer_help'] = 'థీమ్ సమీక్షించాలని క్రింది లింక్పై క్లిక్ చేయండి.';
+$lang['devel_translations'] = 'అనువాదాలు';
+$lang['devel_translation_mode'] = 'అనువాద మోడ్';
+$lang['devel_view_theme'] = 'థీమ్ చూడండి';
+$lang['devel_synchronize_updates'] = 'సమకాలీకరించు నవీకరణలు';
+$lang['devel_synchronization_state_invalid'] = 'సమకాలీకరణ రాష్ట్ర చెల్లదు.';
