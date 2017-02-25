246a5db7d3eaaf6117a9e9373ba47d45a1c52c26
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:17:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'සංවර්ධන මෙවලම්';
+$lang['devel_app_description'] = 'මෙම පිටුව විසින්, තේමාව හා වෙනත් විජට් ඉක්මන් සංවර්ධක දළ විශ්ලේෂණයක් ඉදිරිපත් කරයි.';
+$lang['devel_install_wizard'] = 'විශාරද ස්ථාපනය';
+$lang['devel_install_wizard_help'] = 'ස්ථාපනය තැනීමේ මායා අඳුන ආරම්භ කිරීමට පහත සබැඳිය ක්ලික් කරන්න.';
+$lang['devel_theme_viewer'] = 'තේමාව Viewer';
+$lang['devel_theme_viewer_help'] = 'යන තේමාව සමාලෝචනය කිරීමට පහත සබැඳිය ක්ලික් කරන්න.';
+$lang['devel_translations'] = 'පරිවර්තන';
+$lang['devel_translation_mode'] = 'පරිවර්තක ක්ෂේත්රය';
+$lang['devel_view_theme'] = 'දැක්ම තේමාව';
+$lang['devel_synchronize_updates'] = 'යාවත්කාලීන සමමුහුර්ත කරන්න';
+$lang['devel_synchronization_state_invalid'] = 'සමමුහුර්ත රාජ්ය වලංගු නැත.';
