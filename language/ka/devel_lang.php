e3ab991fdb7c49fca120cb7a1b19b585a5ca989b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:13:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'შემქმნელი ინსტრუმენტები';
+$lang['devel_app_description'] = 'ეს გვერდი უზრუნველყოფს სწრაფ დეველოპერი მიმოხილვა თემა და სხვა widgets.';
+$lang['devel_install_wizard'] = 'Install Wizard';
+$lang['devel_install_wizard_help'] = 'დააწკაპუნეთ ლინკზე დაიწყება ინსტალაცია ოსტატი.';
+$lang['devel_theme_viewer'] = 'Theme Viewer';
+$lang['devel_theme_viewer_help'] = 'დააწკაპუნეთ ლინკზე გადახედოს თემა.';
+$lang['devel_translations'] = 'მეხსიერება';
+$lang['devel_translation_mode'] = 'თარგმანი რეჟიმი';
+$lang['devel_view_theme'] = 'View Theme';
+$lang['devel_synchronize_updates'] = 'სინქრონიზაცია არ დაუყენებია';
+$lang['devel_synchronization_state_invalid'] = 'სინქრონიზაციის სახელმწიფო არასწორია.';
