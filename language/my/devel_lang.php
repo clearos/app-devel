54145fc29c74ce0df74209834b25fd3067dba4a8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 16:10:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/devel_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['devel_app_name'] = 'ရေးသားသူ Tools များ';
+$lang['devel_app_description'] = 'ဤစာမျက်နှာကိုဆောင်ပုဒ်နှင့်အခြား Widgets တွေတစ်ဦးအမြန် developer များခြုံငုံသုံးသပ်ချက်ပေးစွမ်းသည်။';
+$lang['devel_install_wizard'] = 'Wizard ကို Install';
+$lang['devel_install_wizard_help'] = 'အ install wizard ကိုစတင်ဖို့အတွက်အောက်ပါ link ကိုကလစ်နှိပ်ပါ။';
+$lang['devel_theme_viewer'] = 'theme Viewer ကို';
+$lang['devel_theme_viewer_help'] = 'ဆောင်ပုဒ်ပြန်လည်သုံးသပ်ဖို့အောက်ပါ link ကိုကလစ်နှိပ်ပါ။';
+$lang['devel_translations'] = 'ဘာသာပြန်ချက်များ';
+$lang['devel_translation_mode'] = 'ဘာသာပြန်ချက် Mode ကို';
+$lang['devel_view_theme'] = 'ကြည့်ရန်အတွက်အဓိကအကြောင်းအရာ';
+$lang['devel_synchronize_updates'] = 'Updates ကိုထပ်တူကျအောင်';
+$lang['devel_synchronization_state_invalid'] = 'synchronization ပြည်နယ်မမှန်ကန်ဘူး။';
